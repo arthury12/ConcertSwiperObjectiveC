f5ee54e1c983a521669ea19a792b22b11269d32e
Arthur Yu <Arthur.Yu@ticketmaster.com>
2016-02-23 15:10:36 -0800

    Initial Commit

---
+++ concertSwiper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  concertSwiper
+//
+//  Created by Arthur Yu on 2/23/16.
+//  Copyright © 2016 Arthur Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ concertSwiper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  concertSwiper
+//
+//  Created by Arthur Yu on 2/23/16.
+//  Copyright © 2016 Arthur Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
